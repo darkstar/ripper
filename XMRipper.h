7f0b9648ff087a93be67c08aa5063e0f574c7042
Michael Drueing <michael@drueing.de>
2011-01-08 03:04:22 +0100

    Fix license headers. again.

--- XMRipper.h
+++ XMRipper.h
@@ -1,13 +1,10 @@
 /***********************************************************************
-* Copyright 2007 Michael Drueing <michael@drueing.de>
+* Copyright 2007-2010 Michael Drueing <michael@drueing.de>
 *
 * This program is free software; you can redistribute it and/or
 * modify it under the terms of the GNU General Public License as
 * published by the Free Software Foundation; either version 2 of
 * the License or (at your option) version 3 or any later version
-* accepted by the membership of KDE e.V. (or its successor approved
-* by the membership of KDE e.V.), which shall act as a proxy 
-* defined in Section 14 of version 3 of the license.
 *
 * This program is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
